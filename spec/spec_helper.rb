072751090401ea0c0b9e619433820209f78c3715
eXiga <anton.kostenich@hotmail.com>
2015-05-18 04:19:48 +0300

    Add simplecov configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,5 +4,11 @@ require 'rspec'
 require 'coveralls'
 Coveralls.wear!
 
+SimpleCov.formatters = [SimpleCov::Formatter::HTMLFormatter, Coveralls::SimpleCov::Formatter]
+SimpleCov.start do
+  add_filter '/spec/'
+  add_filter '/vendor/'
+end
+
 require 'webmock/rspec'
 WebMock.disable_net_connect!(allow: 'coveralls.io')
